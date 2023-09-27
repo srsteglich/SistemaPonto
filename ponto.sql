09543e264c9d00e0e9a8c589b9c47da5968532f1
Sérgio Renato Steglich <79202041+srsteglich@users.noreply.github.com>
2023-09-26 22:22:22 -0300

    Colocação do Ponto de Registro

--- ponto.sql
+++ ponto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 18-Set-2023 às 23:03
+-- Tempo de geração: 27-Set-2023 às 01:17
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -35,18 +35,36 @@ CREATE TABLE IF NOT EXISTS `pontos` (
   `saida_intervalo` time DEFAULT NULL,
   `retorno_intervalo` time DEFAULT NULL,
   `saida` time DEFAULT NULL,
+  `total_horas` time NOT NULL,
   `usuario_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `usuario_id` (`usuario_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `pontos`
 --
 
-INSERT INTO `pontos` (`id`, `data_entrada`, `entrada`, `saida_intervalo`, `retorno_intervalo`, `saida`, `usuario_id`) VALUES
-(4, '2023-09-07', '16:30:49', NULL, NULL, NULL, 1),
-(5, '2023-09-07', '16:31:38', '16:32:16', NULL, NULL, 3);
+INSERT INTO `pontos` (`id`, `data_entrada`, `entrada`, `saida_intervalo`, `retorno_intervalo`, `saida`, `total_horas`, `usuario_id`) VALUES
+(2, '2023-09-20', '08:00:00', '12:01:00', '13:02:58', '17:58:00', '00:00:00', 5),
+(3, '2023-09-08', '08:08:15', '12:01:00', '13:05:58', '18:16:18', '00:00:00', 3),
+(4, '2023-09-07', '08:00:49', '12:01:00', '13:02:58', '18:16:18', '00:00:00', 1),
+(5, '2023-09-07', '08:00:00', '00:00:12', '13:02:58', '17:55:39', '00:00:00', 3),
+(6, '2023-09-09', '08:00:00', '00:00:12', '13:02:58', '18:26:18', '00:00:00', 3),
+(7, '2023-09-09', '08:00:00', '00:00:12', '13:02:58', '18:16:18', '00:00:00', 3),
+(8, '2023-09-10', '08:03:42', '00:00:12', '13:02:58', '18:16:18', '00:00:00', 3),
+(9, '2023-09-20', '08:30:14', '11:32:57', '13:00:00', '17:00:00', '00:00:00', 3),
+(10, '2023-09-21', '08:00:00', '12:01:00', '13:15:00', '17:10:00', '00:00:00', 3),
+(11, '2023-09-11', '08:03:42', '12:01:00', '13:02:58', '18:16:18', '00:00:00', 3),
+(12, '2023-09-18', '08:13:49', '00:00:12', '13:02:58', '17:16:18', '00:00:00', 3),
+(13, '2023-09-19', '08:03:42', '00:00:12', '13:00:00', '17:58:00', '00:00:00', 3),
+(14, '2023-09-17', '08:00:00', '12:01:00', '13:02:58', '17:58:00', '00:00:00', 3),
+(15, '2023-09-09', '08:03:42', '12:01:00', '13:02:58', '18:16:18', '00:00:00', 1),
+(16, '2023-09-10', '08:03:42', '12:01:00', '13:02:58', '18:16:18', '00:00:00', 1),
+(17, '2023-09-23', '20:14:55', '20:14:57', '20:17:19', '09:14:25', '00:00:00', 3),
+(19, '2022-09-22', '07:15:34', '11:40:02', '13:00:00', '17:58:00', '00:00:00', 3),
+(22, '2023-09-24', '21:58:31', '21:58:45', '21:59:18', '21:59:33', '00:00:00', 3),
+(23, '2023-09-25', '08:30:14', '12:00:12', '13:15:00', '18:26:18', '00:00:00', 3);
 
 -- --------------------------------------------------------
 
@@ -64,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `senha` varchar(220) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tipo` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `usuarios`
@@ -77,7 +95,7 @@ INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `email`, `usuario`, `senha`, `tipo`
 (4, 'Leonardo Pucci Silva ', '00875080952', 'LeonardoPucci@gmail.com', 'LeonardoP', '123', 2),
 (5, 'Jean Piérre Corrêa', '02163084965', 'JeanPiérre@gmail.com', 'Corrêa', '123', 1),
 (6, 'Paula Moretti Borges', '3930102951', 'PaulaMoretti@gmail.com', 'Paula Moretti', '123', 2),
-(7, 'Maicon Pereira ', '04468866969', 'MaiconPereira@gmail.com ', 'Maicon Pereira ', '123', 1),
+(7, 'Maicon P. Pereira ', '0446886600', 'MaiconPPereira@gmail.com', 'MaiconPPereira ', '123', 1),
 (8, 'Rodrigo Dutra ', '00926376985', 'RodrigoDutra@gmail.com', 'Rodrigo Dutra ', '123', 1),
 (9, 'Alexey Lopes Ribeiro', '31240389833', 'Alexey@gmail.com', 'Alexey', '123', 2),
 (10, 'Fernando Albino de Abreu  ', '00703432923', 'FernandoAlbino@gmail.com', 'Fernando Albino', '123', 1),
